66a4363c7bf6aed18c2eeeefa6a3b2fb2b781afd
klopfstein <dimitri.kft@gmail.com>
2020-09-03 16:08:50 +0200

    deserialze json in object

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,20 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\DecoderInterface;
-
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
 {
     /**
      * @Route("/listeRegions", name="listeRegions")
      */
-    public function listeRegions(DecoderInterface $decode)
+    public function listeRegions(DecoderInterface $decode, DenormalizerInterface $denormalizerInterface,SerializerInterface $serializerInterface)
     {
         $mesRegions = file_get_contents('https://geo.api.gouv.fr/regions');
-        $mesRegionsTab = $decode->decode($mesRegions, 'json');
-       
+        $mesRegions = $serializerInterface->deserialize($mesRegions, 'App\Entity\Region[]', 'json');
         return $this->render('api/index.html.twig', [
-            'mesRegions' => $mesRegionsTab
+            'mesRegions' => $mesRegions
         ]);
     }
 }
