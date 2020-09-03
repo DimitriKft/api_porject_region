3b9239f763f1ab4812a66ea3fdb17d52e6c35649
klopfstein <dimitri.kft@gmail.com>
2020-09-03 16:10:19 +0200

    update controller

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,8 +4,6 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\DecoderInterface;
-use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
@@ -13,7 +11,7 @@ class ApiController extends AbstractController
     /**
      * @Route("/listeRegions", name="listeRegions")
      */
-    public function listeRegions(DecoderInterface $decode, DenormalizerInterface $denormalizerInterface,SerializerInterface $serializerInterface)
+    public function listeRegions(SerializerInterface $serializerInterface)
     {
         $mesRegions = file_get_contents('https://geo.api.gouv.fr/regions');
         $mesRegions = $serializerInterface->deserialize($mesRegions, 'App\Entity\Region[]', 'json');
