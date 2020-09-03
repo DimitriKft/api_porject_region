60f6e6085be6d100cc0375729cb4627768e4b6fd
klopfstein <dimitri.kft@gmail.com>
2020-09-03 15:31:06 +0200

    add list api regions

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,16 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\DecoderInterface;
+
 
 class ApiController extends AbstractController
 {
     /**
-     * @Route("/api", name="api")
+     * @Route("/listeRegions", name="listeRegions")
      */
-    public function index()
+    public function listeRegions(DecoderInterface $decode)
     {
+        $mesRegions = file_get_contents('https://geo.api.gouv.fr/regions');
+        $mesRegionsTab = $decode->decode($mesRegions, 'json');
+       
         return $this->render('api/index.html.twig', [
-            'controller_name' => 'ApiController',
+            'mesRegions' => $mesRegionsTab
         ]);
     }
 }
